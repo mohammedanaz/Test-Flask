061f1e4681eb81edff99b0a0705a460b5273d3c0
mohammedanaz <mohammedanaz@gmail.com>
2025-01-16 14:06:43 +0530

    dashboard.html created

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Hello world!!!!</h1>'
+    value = [1, 2, 3, 4, 5]
+    return render_template('index.html', value=value)
 
 
-@app.route('/querry')
-def querry():
-    test = request.args.get('test')
-    return f'{test}'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        print('request.form - ', request.form)
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        return redirect(url_for('dashboard', username=username) )
+    return render_template('login.html')
+
+@app.route('/dashboard', methods=['GET',])
+def dashboard():
+    username = request.args.get('username')
+    return render_template('dashboard.html', username=username)
 
 
 if __name__ == '__main__':
